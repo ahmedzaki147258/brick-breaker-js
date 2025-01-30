edae2ba4fe17030fe056c88a9a8827b1bef9dd85
Abdullah <bmaher.ca@gmail.com>
2025-01-30 15:10:33 +0000

    init the bricks functionalities

---
+++ scripts/bricks/brick.js
@@ -0,0 +1,20 @@
+export class Brick {
+    constructor(x, y, width, height, isBreakable) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.isBreakable = isBreakable;
+        this.lives = isBreakable ? 2 : Infinity;
+        this.visible = true;
+    }
+
+    hit() {
+        if (this.isBreakable && this.visible) {
+            this.lives = max(0, this.lives - 1);
+            if (this.lives === 0) {
+                this.visible = false;
+            }
+        }
+    }
+}
