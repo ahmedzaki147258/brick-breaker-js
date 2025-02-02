76e804b326ddeddbe8cf68e831ed9d6198db44fc
moamen ayman <moamenayman596@gmail.com>
2025-02-02 20:18:11 +0200

    add droplifeicon-method

---
+++ scripts/startGame/lifeicon.js
@@ -0,0 +1,38 @@
+export let score = 0;  
+export let lifeIcons = [];  
+
+// drop lifeicon 
+export function dropLifeIcon(x, y) {
+  let lifeIcon = new LifeIcon(x, y);
+  lifeIcons.push(lifeIcon);
+}
+
+export class LifeIcon {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.speedY = 2;
+  }
+  
+  update() {
+    this.y += this.speedY; // moveing icon to bootom
+  }
+
+  // sure the icon crush with paddle 
+  checkCollision() {
+    if (
+      paddle.x < this.x + 20 && paddle.x + paddle.width > this.x &&
+      paddle.y < this.y + 20 && paddle.y + paddle.height > this.y
+    ) {
+      
+      lives++;
+      lifeIcons = lifeIcons.filter(icon => icon !== this);  
+    }
+  }
+
+  draw() {
+    ctx.fillStyle = 'red';
+    ctx.fillRect(this.x, this.y, 20, 20);
+  }
+}
+
