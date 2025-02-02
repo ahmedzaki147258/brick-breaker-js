bdf37f04e6a8ec3a0d1d659179093e5e2c59fc60
NadaEmamm <nadaemam.me@gmail.com>
2025-02-02 19:15:51 +0200

    Update end game

--- scripts/startGame/dead.js
+++ scripts/startGame/dead.js
@@ -1,11 +1,18 @@
 import { lives } from "./lives.js";
+import { startPage } from "./startedPage.js";
+import { endGame } from "./end.js";
+import { gameResult } from "./Score.js";
 
-export let live = 3;
+export let live = 3
+
 export function Dead() {
-    if (live === 1) {
-        alert("Game Over");
-    } else {
-        --live;
-        lives(live);
+
+    if (live == 0) {
+        startPage();
+        endGame()
+        gameResult();
     }
+    live--
+    lives(live);
 }
+
